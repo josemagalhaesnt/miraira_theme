e90067669d4f90da09337afb2424f73a9c1d6441
Zéh Neto <zneto@live.com>
2019-02-23 22:09:29 -0300

    Miraira Theme v.1.0
    - Esquema de Cores atualizado
    - Menu Principal responsivo
    - Adicionada a página de Arquivo e Erro
    - Adicionada a opção de adicionar logo personalizada
    - Implementada a função de tradução dos textos do Tema

--- index.php
+++ index.php
@@ -1,17 +1,42 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<?php get_header();?>
+	<img class="img-fluid" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+	<div class="content-area">
+		<main>
+			<section class="middle-area">
+				<div class="container">
+					<div class="row">
+						<div class="news col-md-12">	
+							<?php
+							//Se houver algum post
+								if(have_posts()):
+									//Enquanto houver, mostre
+									while(have_posts()): the_post();
+							?>
+							
+							<?php get_template_part('template-parts/content', get_post_format()); ?>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+							<?php 
+								endwhile;
+							?>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+							<div class="row">
+								<div class="pages text-left col-6">
+									<?php previous_posts_link(__('<< Newer posts', 'miraira')); ?>
+								</div>
+								<div class="pages text-right col-6">
+									<?php next_posts_link(__('Older posts >>','miraira')); ?>
+								</div>
+							</div>
+
+							<?php
+								else:	
+							?>
+							<p><?php _e('There is nothing yet to be displayed', 'miraira'); ?></p>
+						<?php endif;?>
+						</div>
+					</div>
+				</div>	
+			</section>
+		</main>
+	</div>
+<?php get_footer();?>
